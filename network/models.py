5f0e1091137186aa5acd750e416d693028b182d1
mohamedadnane8 <mohamed.adnane8@gmail.com>
2020-09-09 22:24:55 +0100

    tweet view function + bootstrap

--- network/models.py
+++ network/models.py
@@ -4,3 +4,50 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="posts")
+
+    description = models.TextField(max_length=600)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def number_likes(self):
+        return self.post_like.all().count()
+
+    def __str__(self):
+        return "Post id: {self.id} owner: {self.owner.username}"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "owner": self.owner.username,
+            "description": self.description,
+            "comments": [comment.serialize() for comment in self.comment.all()],
+            "timestamp": self.date_created.strftime("%b %-d %Y, %-I:%M %p"),
+            "likes": self.number_likes(),
+        }
+
+
+class Like(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="user_likes")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, related_name="post_like")
+
+    # User should not be able to like the same post multiple times
+    class Meta:
+        unique_together = ["post", "owner"]
+
+
+class Comment(models.Model):
+    the_comment = models.CharField(max_length=200)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="user_comment", null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, default=None, related_name="comment")
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "owner": self.owner.username,
+            "the_comment": self.the_comment,
+            "date_created": self.date_created.strftime("%b %-d %Y, %-I:%M %p"),
+        }
--- network/views.py
+++ network/views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
-
-from .models import User
+import json
+from .models import *
 
 
 def index(request):
@@ -61,3 +61,21 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+def tweet(request):
+    # Composing a new email must be via POST
+    if request.method != "POST":
+        return JsonResponse({"error": "POST request required."}, status=400)
+
+    data = json.loads(request.body)
+    # Get the content of the tweet
+    description = data.get("description", "")
+
+    if description == "" or description.isspace():
+        #TODO: have to check also if the file has only spaces!
+        return JsonResponse({"message": "Email sent successfully."}, status=400)
+
+    post = Post(owner = request.user,description=description)
+    post.save()
+
+    return JsonResponse({"message": "Email sent successfully."}, status=201)
